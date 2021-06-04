7b482db763296ffc725b1211586e638837587492
chenxi5 <chenxi5@01zhuanche.com>
2021-06-04 22:03:18 +0800

    modify sxpress instance

--- index.js
+++ index.js
@@ -10,14 +10,9 @@ function Sxpress() {
   this._request = null; // 存储请求体
   this._response = null; // 存储响应体
   this._httpServer = null; // 存储http服务
-  this._routerInstance = new SxpressRouter(); // 存储路由实例对象
+  this._routerInstance = new SxpressRouter();  // 存储路由实例对象
   this._middlewareList = []; // 存储中间件函数
-  this._middlewareIndex = 0; // 存储中间件计数
-  this._ended = false; // 是否返回数据
-  this._async = false; // 是否异步处理
-  this._responseCode = 200; // 响应体code
-  this._responseHeaders = {}; // 响应体headers
-  this._responseCookies = []; // 响应体cookies
+  this._middlewareIndex = 0;  // 存储中间件计数
 }
 
 // 初始化
@@ -25,18 +20,13 @@ Sxpress.prototype.init = function (req, res) {
   this._request = req;
   this._response = res;
   this._response.send = this.send;
-  this._ended = false;
-  this._async = false;
   this._middlewareIndex = 0;
   this._routerInstance.init(this._request, this._response);
-  this._responseCode = 200;
-  this._responseHeaders = {};
-  this._responseCookies = [];
 };
 
 // 创建http服务
 Sxpress.prototype.createServer = function () {
-  var self = this;
+  var that = this;
 
   this._httpServer = http.createServer((req, res) => {
     // 处理favicon.ico
@@ -44,31 +34,38 @@ Sxpress.prototype.createServer = function () {
       return res.end('');
     }
 
-    // 初始化Sxpress
-    self.init(req, res);
-
-    // 运行中间件
-    if (self._middlewareList.length) {
-      self.next();
+    // 初始化res._sxpress
+    res._sxpress = {
+      code: 200,  // 响应体code
+      ended: false,  // 是否返回数据
+      async: false,  // 是否异步处理
+      headers: {},  // 响应体headers
+      cookies: []  // 响应体cookies
     }
 
-    if (self._ended) {
+    // 初始化Sxpress
+    that.init(req, res);
+
+    // 运行中间件
+    if (that._middlewareList.length) {
+      that.next();
+    }
+
+    // 请求结束，不继续处理
+    if (that._response._sxpress.ended) {
       return false;
     }
 
     // 处理路由
-    if (self._routerInstance._routerList.length) {
-      var urlParse = url.parse(req.url);
+    if (that._routerInstance._routerList.length) {
+      var urlParse = url.parse(that._request.url);
       var pathname = urlParse.pathname;
       var router = null;
 
       // 查找路由
-      for (var i = 0; i < self._routerInstance._routerList.length; i++) {
-        if (
-          self._routerInstance._routerList[i].method === req.method &&
-          self._routerInstance._routerList[i].path === pathname
-        ) {
-          router = self._routerInstance._routerList[i];
+      for (var i = 0; i < that._routerInstance._routerList.length; i++) {
+        if (that._routerInstance._routerList[i].method === that._request.method && that._routerInstance._routerList[i].path === pathname) {
+          router = that._routerInstance._routerList[i];
 
           break;
         }
@@ -76,11 +73,11 @@ Sxpress.prototype.createServer = function () {
 
       if (router) {
         // 查找到路由
-        return router.handler(self._request, self._response);
-      } else if (!router && !self._async) {
+        return router.handler(that._request, that._response);
+      } else if (!router && !that._response._sxpress.async) {
         // 未查找到路由, 且不是异步
         return res.send('Not Found 404');
-      } else if (self._async) {
+      } else if (that._response._sxpress.async) {
         // 异步不处理
         return false;
       } else {
@@ -95,15 +92,16 @@ Sxpress.prototype.createServer = function () {
 
 // 处理下一步
 Sxpress.prototype.next = function () {
-  var self = this;
+  var that = this;
 
-  if (this._ended) return false;
+  // 已结束不继续下一步
+  if (this._response._sxpress.ended) return false;
 
   var middleware = this._middlewareList[this._middlewareIndex++];
 
   if (middleware && typeof middleware === 'function') {
     middleware.call(this, this._request, this._response, function () {
-      self.next.call(self);
+      that.next.call(that);
     });
   }
 };
@@ -158,12 +156,12 @@ Sxpress.prototype.delete = function (path, callback) {
 
 // 输出函数
 Sxpress.prototype.send = function (data) {
-  var sxpressInstance = global.sxpressInstance;
-  var responseCookies = sxpressInstance._responseCookies;
-  var code = sxpressInstance._responseCode;
-  var headers = sxpressInstance._responseHeaders;
+  // this指向response对象
+  var responseCookies = this._sxpress.cookies;
+  var code = this._sxpress.code;
+  var headers = this._sxpress.headers;
   var cookie;
-  var cookies = []; // cookies数组
+  var cookies = [];  // cookies数组
   var cookieItems = [];
 
   this._ended = true;
@@ -172,7 +170,7 @@ Sxpress.prototype.send = function (data) {
   if (responseCookies.length) {
     for (var i = 0; i < responseCookies.length; i++) {
       cookie = responseCookies[i];
-      cookieItems = [];
+      cookieItems = []
 
       cookieItems.push(cookie.key + '=' + cookie.value);
 
@@ -214,7 +212,7 @@ Sxpress.prototype.send = function (data) {
 // 创建实例
 if (!global.sxpressInstance) {
   global.sxpressInstance = new Sxpress();
-
+  
   // 创建http服务
   global.sxpressInstance.createServer();
 }
--- middlewares/static.js
+++ middlewares/static.js
@@ -11,41 +11,41 @@ var MIME_TYPES = {
 };
 
 module.exports = function (req, res, next) {
-  var self = this;
+  var that = this;
 
   // 获取请求文件名
   var extname = path.extname(url.parse(req.url).pathname);
 
   // 处理静态资源，配置过静态资源目录，请求路径为文件类型，在MIME_TYPES有命中
-  if (self._static && extname && MIME_TYPES[extname]) {
+  if (that._static && extname && MIME_TYPES[extname]) {
     // 设置异步标识
-    self._async = true;
+    res._sxpress.async = true;
 
     // 解析文件路径
     var pathParseUrl = path.parse(req.url);
     // 拼接文件路径
     var filePath = path.join(
-      self._static,
+      that._static,
       pathParseUrl.dir,
       pathParseUrl.name + extname
     );
 
     fs.access(filePath, function (error) {
-      self._ended = true;
+      that._ended = true;
 
       // 文件不存在返回404
       if (error && error.code === 'ENOENT') {
-        return self._response.end('Not Found 404');
+        return that._response.end('Not Found 404');
       }
 
       // 创建读取流
       const readStream = fs.createReadStream(filePath);
 
       // 设置输出content-type
-      self._response.writeHead(200, { 'content-type': MIME_TYPES[extname] });
+      that._response.writeHead(200, { 'content-type': MIME_TYPES[extname] });
 
       // 通过管道输出流
-      readStream.pipe(self._response);
+      readStream.pipe(that._response);
     });
   } else {
     next();
